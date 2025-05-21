48e7ef751e8f3cc0fab72ec506fc0385e20ee306
Chris Shaw <me@cshaw.xyz>
2025-05-22 00:06:22 +1000

    Update redirect route

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
 
